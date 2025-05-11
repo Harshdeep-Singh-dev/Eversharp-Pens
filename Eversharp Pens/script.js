644904fe8b69a528ce09e0973b5f6d693ce38e55
Harshdeep-Singh-dev <harshdeep2965@gmail.com>
2025-05-11 00:50:45 -0400

    Better Navbar and comments

--- Eversharp Pens/script.js
+++ Eversharp Pens/script.js
@@ -1,10 +1,12 @@
 // The purpose ofthe javascript is to change background Color of products section when specific product is hovered upon
 // Initialize Lenis
+console.log(window.innerWidth)
+console.log(window.innerHeight)
 const lenis = new Lenis({
     autoRaf: true,
   });
   
-  // Listen for the scroll event and log the event data
+  // LENIS INSTALLED
   lenis.on('scroll', (e) => {
     console.log(e);
   });
@@ -13,14 +15,14 @@ let products = document.querySelector("#products");
 let boxes = document.querySelectorAll(".box figure");
 
 
-// Setting background color
+// SETTING PASTEL COLORS
 const setBackgroundColor = (i) => {
   var style = window.getComputedStyle(boxes[i]);
   var backGroundColor = style.backgroundColor;
   products.style.backgroundColor = backGroundColor;
 };
 
-// Resetting Background Color
+// CHANGING PASTEL COLORS
 
 const resetBackgroundColor = () => {
   products.style.backgroundColor = "#fff";
@@ -31,11 +33,24 @@ for (let i = 0; i < Math.min(8, boxes.length); i++) {
   boxes[i].addEventListener("mouseleave", resetBackgroundColor);
 }
 
-// Hamburg logo for mobile phones
-  const hamburger = document.getElementById('hamburger');
-  const navMenu = document.getElementById('navMenu');
+// HAMBURG SCRIPT
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("navMenu");
 
-  hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
+  // TOGGLING
+  hamburger.addEventListener("click", (e) => {
+    e.stopPropagation(); // prevent bubbling to document
+    navMenu.classList.toggle("active");
   });
+
+  // STOP CLICKS INSIDE TO PREVENT COLLAPSE
+  navMenu.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+
+  // COLLAPSE ON ANYWHERE ELSE CLICKED
+  document.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  });
+
 
